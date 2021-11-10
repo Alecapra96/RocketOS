243caa71f004ec28219f51aa135ce9eb42c30f92
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-11-10 19:53:33 -0300

    Arreglo errores de copy - paste xD

--- rocket.sh
+++ rocket.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 sudo apt -y install dialog
-# creamos la funcion DIALOG 
-DIALOG=${DIALOG=dialog}
 
 
-DIALOG --infobox "Bienvenido a RocketOS" 0 0 
+dialog --infobox "Bienvenido a RocketOS" 0 0 
 
 
 #Actualizo el sistema
@@ -12,7 +10,7 @@ sudo apt -y install && sudo apt -y upgrade
 sudo apt --fix-missing update
 
 #|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
+DIALOG=${DIALOG=dialog}
 tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
 trap "rm -f $tempfile" 0 1 2 5 15
 $DIALOG --backtitle "Creado por : linkedin.com/in/alejandro-capra" \
@@ -20,7 +18,7 @@ $DIALOG --backtitle "Creado por : linkedin.com/in/alejandro-capra" \
         --radiolist "Selecciona con la tecla space la distro que deseas" 17 100 10 \
         "1"    "Debian 11" off \
         "2"    "Ubuntu" off \
-        "3"   s "Arch" off  2> $tempfile
+        "3"    "Arch" off  2> $tempfile
 retval=$?
 choice=`cat $tempfile`
 case $retval in
@@ -33,9 +31,12 @@ case $retval in
             echo "Instalar RocketOS sobre debian"
             
                 #Instalo dependencias bases
-                sudo apt -y install --no-install-suggests --no-install-recommends xserver-xorg-core software-properties-common broadcom-sta-dkms cmake libfreetype6-dev libfontconfig1-dev xclip build-essential libx11-dev libxft-dev build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev x11-xserver-utils
+                sudo apt -y install --no-install-suggests --no-install-recommends wireles-tools xserver-xorg-core software-properties-common broadcom-sta-dkms cmake libfreetype6-dev libfontconfig1-dev xclip build-essential libx11-dev libxft-dev build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev x11-xserver-utils
+                #iNCRLUIR REPOSITORIOS NON FREE A DEBIAN
+                
+                #INSTALAR BROADCOM 
+                https://debian.pkgs.org/11/debian-nonfree-arm64/broadcom-sta-dkms_6.30.223.271-17_all.deb.html
                 #Instalo BSPWM
-                sleep 5 
                 git clone https://github.com/baskerville/bspwm.git
                 git clone https://github.com/baskerville/sxhkd.git
                 cd bspwm/
@@ -44,25 +45,23 @@ case $retval in
                 cd ../sxhkd/
                 make
                 sudo make install
-                sleep 5 
+                
                 sudo apt -y install bspwm
-                sleep 5 
                 #Creo Los archivos de configuracion
                 mkdir ~/.config
                 mkdir ~/.config/bspwm
                 mkdir ~/.config/sxhkd
-                sleep 5 
                 cd ../bspwm/
                 cp examples/bspwmrc ~/.config/bspwm/
                 chmod +x ~/.config/bspwm/bspwmrc
                 cp examples/sxhkdrc ~/.config/sxhkd/
-                sleep 5 
+                 
                 #Instalo el script de startx
                 sudo apt -y install xinit
-                sleep 5 
+            
                 #Creo el archivo .xinitrc
-                sudo cp ~/RocketOS-script/.xinitrc ~/
-                sleep 5 
+                sudo cp ~/RocketOS/.xinitrc ~/
+                
           
             ;;
         2)
